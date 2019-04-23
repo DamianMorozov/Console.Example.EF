2073c622a326f8d4ad0e86159f2a47da94ca8afd
Morozov Damian <damian@morozov33.ru>
2019-04-23 16:24:29 +0300

    Make Sql connect

--- Console.Example.EF/Program.cs
+++ Console.Example.EF/Program.cs
@@ -1,15 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Console.Example.EF
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            var builder = CreateSqlConnection();
+            SqlConnect(builder);
+
+            System.Console.WriteLine();
+            System.Console.WriteLine("Press any key to finish...");
+            System.Console.ReadKey(true);
         }
+
+        private static SqlConnectionStringBuilder CreateSqlConnection()
+        {
+            System.Console.WriteLine("Create new SqlConnectionStringBuilder.");
+            var builder = new SqlConnectionStringBuilder() {
+                Authentication = SqlAuthenticationMethod.ActiveDirectoryIntegrated,
+                //UserID = Environment.UserName,
+                DataSource = "tcp:localhost",
+                InitialCatalog = "master",
+                ConnectTimeout = 30,
+                TrustServerCertificate = true,
+            };
+            System.Console.WriteLine($"  builder.Authentication: {builder.Authentication}.");
+            System.Console.WriteLine($"  builder.DataSource: {builder.DataSource}.");
+            System.Console.WriteLine($"  builder.InitialCatalog: {builder.InitialCatalog}.");
+            return builder;
+        }
+
+        private static void SqlConnect(SqlConnectionStringBuilder builder)
+        {
+            System.Console.WriteLine("Sql try connect.");
+            try
+            {
+                using (var con = new SqlConnection(builder.ConnectionString))
+                {
+                    System.Console.WriteLine("  Sql is connected.");
+                    con.Open();
+                    System.Console.WriteLine("  Sql connect is opened.");
+
+
+                }
+
+            }
+            catch (SqlException exception)
+            {
+                System.Console.WriteLine($"  Sql error: {exception.Message}!");
+            }
+        }
+
+        
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Console example of Entity Framework.
 
 **[Original manual.](https://www.microsoft.com/en-us/sql-server/developer-get-started/csharp/win/step/2.html)**
+
+**.NET FrameWork 4.7**
+**C# 7.3**

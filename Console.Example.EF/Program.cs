0acfd2a6ea16743aa98a42020744c1286376d99d
Morozov Damian <damian@morozov33.ru>
2019-04-23 14:58:18 +0300

    Add Console.Example.EF

---
+++ Console.Example.EF/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Console.Example.EF
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            System.Console.ReadKey();
+        }
+    }
+}
